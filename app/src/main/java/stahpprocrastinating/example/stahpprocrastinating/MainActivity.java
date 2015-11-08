582c58d2d9ff9d9e1b4a2655ec5b02bf34e43616
Anirudh Mukundan <m.anirudh94@gmail.com>
2015-11-07 21:19:06 -0500

    rebased braintree payment api

--- app/src/main/java/stahpprocrastinating/example/stahpprocrastinating/MainActivity.java
+++ app/src/main/java/stahpprocrastinating/example/stahpprocrastinating/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.braintreepayments.api.Braintree;
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -15,17 +17,27 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
 
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends FragmentActivity {
 
     private LoginButton loginBtn;
     private TextView username;
     private CallbackManager callbackManager;
+
+    private static final int PAYMENT_AUTH_REQ = 100;
+    private String clientToken;
+    private boolean isBraintreeSetup = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +51,7 @@ public class MainActivity extends FragmentActivity {
                 onFbLogin();
             }
         });
+        getClientToken();
     }
 
     private void onFbLogin() {
@@ -74,10 +87,6 @@ public class MainActivity extends FragmentActivity {
                                             } catch (JSONException e) {
                                                 e.printStackTrace();
                                             }
-                                            finally {
-                                                Intent goNext = new Intent(getBaseContext(), ListGoalsActivity.class);
-                                                startActivity(goNext);
-                                            }
                                         }
                                     }
 
@@ -101,11 +110,78 @@ public class MainActivity extends FragmentActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYMENT_AUTH_REQ) {
+            if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+                String nonce = data.getStringExtra(
+                        BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE
+                );
+                postNonceToServer(nonce);
+
+                Intent goNext = new Intent(getBaseContext(), ListGoalsActivity.class);
+                startActivity(goNext);
+            }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
+            // TODO: CHECK IF USER IS IN DATABASE ALREADY
+            //call paypal next
+            onBraintreeSubmit();
+        }
     }
 
+    public void getClientToken() {
+        AsyncHttpClient client = new AsyncHttpClient();
 
+        TextHttpResponseHandler handler = new TextHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String clientToken) {
+                MainActivity.this.clientToken = clientToken;
+                Braintree.setup(MainActivity.this, clientToken, new Braintree.BraintreeSetupFinishedListener() {
+                    @Override
+                    public void onBraintreeSetupFinished(boolean setupSuccessful, Braintree braintree, String errorMessage, Exception exception) {
+                        if (setupSuccessful) {
+                            isBraintreeSetup = true;
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                // Do nothing.
+            }
+        };
+
+        client.get("http://10.0.2.2:3000/client_token", handler);
+    }
+
+    void postNonceToServer(String nonce) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("payment_method_nonce", nonce);
+        client.post("http://10.0.2.2:3000/checkout", params,
+                new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        //TODO: Handle response
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        // Do nothing
+                    }
+                }
+        );
+    }
+
+    public void onBraintreeSubmit() {
+        //ignore request until braintree is setup
+        if (isBraintreeSetup) {
+            Intent intent = new Intent(getApplicationContext(), BraintreePaymentActivity.class);
+            intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, clientToken);
+            startActivityForResult(intent, PAYMENT_AUTH_REQ);
+        }
+    }
 }
